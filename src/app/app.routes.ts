6d8d7ede379b4452991cf90a39a25ca4317a9909
jesus <sirijesus754@gmail.com>
2020-08-16 10:16:29 -0300

    Guard implementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,13 @@ import { HomeComponent } from './components/home/home.component';
 import { PreciosComponent } from './components/precios/precios.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
 import { RegistroComponent } from './components/auth/registro/registro.component'
-
+import { AuthGuard } from './components/auth/guards/auth.guard';
 
 const APP_ROUTES: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'registrarse', component: RegistroComponent },
-    { path: 'precios', component: PreciosComponent },
-    { path: 'protegida', component: ProtegidaComponent },
+    { path: 'home', component: HomeComponent,canActivate:[!AuthGuard]},
+    { path: 'registrarse', component: RegistroComponent,canActivate:[!AuthGuard] },
+    { path: 'encargo', component: PreciosComponent,canActivate:[AuthGuard]},
+    { path: 'pedidos', component: ProtegidaComponent,canActivate:[AuthGuard] },
     { path: '**', pathMatch: 'full', redirectTo: ''} 
 ]
 
